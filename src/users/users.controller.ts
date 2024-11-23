2f1d79c35e54e5b0e6de5d42c5c717653fe3c9f7
Juan Alberto <jarscode.dev@outlook.com>
2024-11-23 16:36:39 +0100

    progress

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,14 @@
 /* eslint-disable prettier/prettier */
-import { Controller } from '@nestjs/common'
+import { Controller, Body, Post } from '@nestjs/common'
+import { UsersService } from './users.service'
 
 
 @Controller('users')
 export class UsersController {
+  constructor(private userService: UsersService) {}
 
+  @Post('register')
+  async register(@Body() body) {
+    return this.userService.register(body)
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,63 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import bcrypt from 'bcrypt'
+import { validationErrors } from '../utils/validations'
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+  constructor(private readonly prisma: PrismaService) {}
+
+  static async existingUser(email: string) {
+
+    try {
+      const user = await this.prisma.users.findUnique({
+        where: { email }
+      })
+
+      return !!user // This line return true if user exists & false if not
+    }
+
+    catch (error) {
+      throw new Error(`Error checking user existence: ${error}`)
+    }
+
+  }
+
+  static async register(name: string, email: string, password: string) {
+
+    try {
+
+      const validate = validationErrors(email, password)
+
+      if (Object.keys(validate).length > 0) {
+        return validate
+      }
+
+      const existingUser = await this.existingUser(email)
+
+      if (existingUser) {
+        throw new Error('User already exists')
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10)
+
+      const user = await this.prisma.users.create({
+        data: {
+          full_name: name,
+          email,
+          password: hashedPassword
+        }
+      })
+
+      return user
+    }
+
+    catch (error) {
+      throw new Error(`Error creating user: ${error}`)
+    }
+
+  }
+
+}
