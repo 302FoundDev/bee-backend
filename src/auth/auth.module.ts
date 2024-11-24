7795a48f77b2ebb37a2f31690b47be456e8c5bff
Juan Alberto <jarscode.dev@outlook.com>
2024-11-24 20:43:52 +0100

    update auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Module } from '@nestjs/common'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
+import { AuthService } from './auth.service'
+import { PrismaService } from 'src/prisma.service'
+import { AuthController } from './auth.controller'
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET!,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, PrismaService],
+  exports: [AuthService],
 })
+
 export class AuthModule {}
