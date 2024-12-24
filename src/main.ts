58e0404df2acd23b411578316e4ae2b947607d69
Juan Alberto <jarscode.dev@outlook.com>
2024-12-24 17:50:30 +0100

    new port

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   const documentFactory = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
-  const port = process.env.PORT || 8000;
+  const port = process.env.PORT || 5000;
   await app.listen(port, () => {
     console.log(`Application is running on: http://localhost:${port}`);
   });
